5dc237f605642162a2023269211dd7d486532da3
Bugaenko Andrew <849-bap19u@users.noreply.git.iu7.bmstu.ru>
2020-09-21 00:58:59 +0300

    Another revert attempt

--- Assets/Scripts/ScoreTracker.cs
+++ Assets/Scripts/ScoreTracker.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ScoreTracker : MonoBehaviour
 {
     public Text scoreText;
+    public Text HighScoreText;
 
     public int Score { get; private set; } = 0;
 
@@ -19,11 +20,25 @@ public class ScoreTracker : MonoBehaviour
     private void Start()
     {
         scoreText.text = "0";
+        HighScore = PlayerPrefs.GetInt("HighScore", 0);
+        HighScoreText.text = HighScore.ToString();
     }
 
     public void IncreaseScore(int addScore)
     {
         Score += addScore;
         scoreText.text = Score.ToString();
+        HighScore = Mathf.Max(HighScore, Score);
+
+        ScoreText.text = Score.ToString();
+        HighScoreText.text = HighScore.ToString();
+
+        PlayerPrefs.SetInt("HighScore", HighScore);
+    }
+
+    public void ResetScore()
+    {
+        Score = 0;
+        ScoreText.text = Score.ToString();
     }
 }
